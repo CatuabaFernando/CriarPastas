f5f2dc6e45e11437d3ad6caef602b8b788eccbd3
Fernando Catuaba <fernandocatuaba1@gmail.com>
2022-04-11 17:17:22 -0300

    Criando pastas

---
+++ TesteDeListas/Pastas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TesteDeListas
+{
+    class Pastas
+    {
+        public static void CriarPastas(List<string> pathFile)
+        {
+            string s = String.Join(@"\", pathFile);// Cria uma string formada pelos elementos da lista separados pelo caractere "\".
+
+            if (Directory.Exists(s.Remove(s.LastIndexOf(@"\"))))// Verifica se o caminho onde a pasta vai ser criada existe.
+            {
+                if (!Directory.Exists(s))
+                {
+                    string nomeDaPasta;
+
+                    nomeDaPasta = s.Substring(s.LastIndexOf(@"\") + 1);// Insere o nome da ultima pasta criada na string nomeDaPasta.
+
+                    Directory.CreateDirectory(s);
+                    Console.Write("Pasta '{0}' criada \n", nomeDaPasta);// Retorna o nome da pasta criada.
+                }
+                else
+                {
+                    Console.WriteLine("Pasta já existe. \n");
+                }
+            }
+            else
+            {
+                Console.Write("Caminho não existe. \n");
+            }
+        }
+    }
+}
---
+++ TesteDeListas/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TesteDeListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> caminho = new List<string>();// Cria uma lista de strings.
+
+            Console.Write("Digite o caminho da pasta: ");
+
+            caminho.Add(Console.ReadLine());
+
+            Console.Write("Digite o nome da pasta: ");
+
+            caminho.Add(Console.ReadLine());
+
+            Console.Write("");
+
+            Pastas.CriarPastas(caminho);// Chamada do método CreateFiles da classe CriarPastas passando a string s como parâmetro.
+
+            if (Directory.Exists(String.Join(@"\", caminho)))// Verifica se alguma pasta foi criada ou se já existia alguma pasta com o nome digitado.
+            {
+                string resposta = "";
+
+                while (resposta != "s" && resposta != "n")
+                {
+                    Console.Write("Deseja crirar subpasta? s(sim) n(não) : ");
+                    resposta = Console.ReadLine();
+
+                    switch (resposta)
+                    {
+                        case "s":
+                            Console.Write("Digite o nome da pasta: ");
+                            caminho.Add(Console.ReadLine());
+                            Pastas.CriarPastas(caminho);// Chamada do método CreateFiles da classe CriarPastas passando a string s como parâmetro.
+                            resposta = "";
+                            break;
+
+                        case "n":
+                            break;
+
+                        default:
+                            Console.Write("Digite s (sim ) ou n (não).\n");
+                            break;
+                    }
+                }
+            }
+            Console.Write("\n Digite qualquer coisa para sair.");
+            Console.ReadKey();
+        }
+    }
+}
